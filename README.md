e907f7c99cc1c457d67475bb475269d4a7ee334b
cs <cs.temporary@icloud.com>
2019-02-01 03:19:29 +0000

    Optionally check runtime

--- README.md
+++ README.md
@@ -5,18 +5,19 @@ passing if it matches the output of a specified solution executable.
 
 ## Usage
 
-* `./comp_soln.sh [-v] [-h] to_test solution tests`
+* `./comp_soln.sh [-vth] to_test solution tests`
     * `to_test`: The executable to test
     * `solution`: The solution executable whose output is considered correct
     * `tests`: File that contains one test per line. Each test should consist of
       space-separated arguments to provide to the executable.
-* `./comp_soln.sh [-v] [-h] tests`
+* `./comp_soln.sh [-vth] tests`
     * `tests`: File that contains one test per line. Each test should be in the
       following format: `<test_executable> <arg1> <arg2> ...`. The solution
       executable is expected to be found at `samples/<test_executable>_soln`.
 * Flags
     * `-v`: checks that `valgrind` finds no errors (`valgrind` must be
       installed)
+    * `-t`: checks that `to_test` runs at most 3x slower than `solution`
     * `-h`: displays usage information
 
 In files containing tests, lines beginning with `#` and blank lines are ignored.
--- comp_soln.sh
+++ comp_soln.sh
@@ -54,19 +54,56 @@ test_valgrind() {
     }; fi
 }
 
+# Usage: test_exec soln_exec args
+# Output stored in test_time, soln_time, and factor (test / soln)
+test_runtime() {
+    test_exec=$1
+    soln_exec=$2
+    args=$3
+
+    test_start="$(date +%s)"
+    eval "$test_exec $args" 2>&1 > /dev/null
+    test_end="$(date +%s)"
+
+    soln_start="$(date +%s)"
+    eval "$soln_exec $args" 2>&1 > /dev/null
+    soln_end="$(date +%s)"
+
+    test_time=$((test_end - test_start))
+    soln_time=$((soln_end - soln_start))
+
+    if [[ soln_time -eq 0 ]]; then {
+        soln_time=1
+    }; fi
+
+    factor=$((test_time / soln_time))
+
+    if [[ $factor -gt 3 ]]; then {
+        return 1;
+    } else {
+        return 0;
+    } fi;
+}
+
+
 run_valgrind_tests=false
+check_runtime=false
 
 # Parse arguments
 # SOURCE: http://wiki.bash-hackers.org/howto/getopts_tutorial
-while getopts ":vh" opt; do
+while getopts ":vth" opt; do
   case $opt in
     v)
       run_valgrind_tests=true
       ;;
+    t)
+      check_runtime=true
+      ;;
     h)
-      echo "Usage: ./comp_soln.sh [-v] to_test solution tests"
+      echo "Usage: ./comp_soln.sh [-vt] to_test solution tests"
       echo "       ./comp_soln.sh [-v] tests"
       echo "-v checks that valgrind finds no errors"
+      echo "-t checks that test runs at most 3x slower than solution"
       echo "-h displays this help text"
       exit 0
       ;;
@@ -162,6 +199,19 @@ for p in "${test_lines[@]}"; do {
             print_color "$red" "FAILED"
             echo "Valgrind Errors:"
             print_color "$gray" "$valgrind_errors"
+        }; fi
+    }; fi
+
+    if $check_runtime; then {
+        echo -n "[$test_num of $num_tests] Checking Runtime: $to_test $args ... "
+        if test_runtime "$to_test" "$solution" "$args"; then {
+            print_color "$green" "OK"
+        } else {
+            failed=true
+            print_color "$red" "FAILED"
+            print_color "$gray" "Test Time: $test_time"
+            print_color "$gray" "Solution Tiime: $soln_time"
+            print_color "$gray" "Factor (test/soln): $factor"
         }; fi
     }; fi
 
